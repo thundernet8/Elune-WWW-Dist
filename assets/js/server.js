57e48a167b6e0b9d381060a06e330d26988b197d
wuxueqian <wuxueqian2010@hotmail.com>
2017-10-01 00:01:57 +0800

    ☘️ Auto distribute resources

--- assets/js/server.js
+++ assets/js/server.js
@@ -70591,17 +70591,21 @@ exports.default = (0, _reactClickOutside2.default)(Select);
 
 
 exports.reset = function (css) {
-  var style = document.createElement('style');
+    var isServer = typeof window === "undefined";
+    if (isServer) {
+        return;
+    }
+    var style = document.createElement("style");
 
-  style.type = 'text/css';
+    style.type = "text/css";
 
-  if (style.styleSheet) {
-    style.styleSheet.cssText = css;
-  } else {
-    style.appendChild(document.createTextNode(css));
-  }
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
 
-  (document.head || document.getElementsByTagName('head')[0]).appendChild(style);
+    (document.head || document.getElementsByTagName("head")[0]).appendChild(style);
 };
 
 /***/ }),
