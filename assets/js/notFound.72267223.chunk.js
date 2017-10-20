6f5d1b4d28dc61f270cb39621e4318ea296d2f3c
吴学谦 <wuxueqian@ezbuy.com>
2017-10-20 16:19:42 +0800

    ☘️ Auto distribute resources

--- assets/js/notFound.72267223.chunk.js
+++ assets/js/notFound.72267223.chunk.js
@@ -1,5 +1,5 @@
 /*!
- * Generated on Fri Oct 20 2017 15:42:36 GMT+0800 (CST)
+ * Generated on Fri Oct 20 2017 16:16:54 GMT+0800 (CST)
  * 
  * Copyright 2017-present, WuXueqian. All rights reserved.
  * 
