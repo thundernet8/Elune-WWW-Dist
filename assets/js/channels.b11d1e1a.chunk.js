2cc8a3cda19ab7203d56ce5e70a66afffc7b8973
wuxueqian <wuxueqian2010@hotmail.com>
2017-11-09 23:05:16 +0800

    ☘️ Auto distribute resources

--- assets/js/channels.b11d1e1a.chunk.js
+++ assets/js/channels.b11d1e1a.chunk.js
@@ -1,5 +1,5 @@
 /*!
- * Generated on Thu Nov 09 2017 15:32:05 GMT+0800 (CST)
+ * Generated on Thu Nov 09 2017 23:02:54 GMT+0800 (CST)
  * 
  * Copyright 2017-present, WuXueqian. All rights reserved.
  * 
